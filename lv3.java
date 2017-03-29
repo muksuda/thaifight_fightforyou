f4a3976482cfafee185e71c1e51bea0a29b21f5e
muksuda <mukink1089@hotmail.com>
2017-03-29 14:39:07 +0700

    fix bug in game

---
+++ lv3.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class lv3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class lv3 extends botton
+{
+    /**
+     * Act - do whatever the lv3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        checkClick();
+        change();
+    }    
+    public void checkClick(){
+        if(Greenfoot.mouseClicked(this)){
+            Greenfoot.playSound("click.wav");
+            Greenfoot.setWorld(new LevelThree());
+        } 
+    }   
+    public void change(){
+        if(Greenfoot.mouseMoved(this)){
+            setImage("levbot30.png");
+        }else{
+            setImage("levbot3.png");
+        }
+    }
+}
